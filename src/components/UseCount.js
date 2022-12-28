dad90b32d32971132533aa8d25e455f1e13ccbec
Sarthak-Srivastava-71449 <113346239+Sarthak-Srivastava-71449@users.noreply.github.com>
2022-12-28 17:22:40 +0530

    Add files via upload

---
+++ src/components/UseCount.js
@@ -0,0 +1,18 @@
+import React, { useState} from 'react';
+
+function Count() {
+   const  [count, setCount] = useState(0)
+
+   
+  return (
+    <div>
+    <div>{count}</div>
+    <div>
+    <button onClick={()=>setCount(count+1)}>Increment</button>
+    <button onClick={()=>setCount(count-1)}>Decrement</button>
+    </div>
+    </div>
+  )
+}
+
+export default Count
